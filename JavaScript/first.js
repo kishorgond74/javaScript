6d4ac04f8b882e838b29d18aef97f484e58ef97f
kishorgond74 <158150316+kishorgond74@users.noreply.github.com>
2025-04-19 06:49:03 +0000

    datatypes

---
+++ JavaScript/first.js
@@ -0,0 +1,15 @@
+console.log("Kishor")
+console.log(3 + 3)
+
+// number => 2 to ower 53
+//bigint
+// string => ""
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Kishor") // string
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
