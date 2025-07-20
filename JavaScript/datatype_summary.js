4cc5fdd5aa600b53ea40deb142531041bbc2742e
kishorgond74 <158150316+kishorgond74@users.noreply.github.com>
2025-07-20 17:02:49 +0000

    datatype_summary.js

---
+++ JavaScript/datatype_summary.js
@@ -0,0 +1,20 @@
+// primitive
+
+// 7 types : string,number, boolean, null, undefined, symbol, bigint,
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+// Reference type (Non primitive)
+
+// Array, Objects, Functions
