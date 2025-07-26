423d06770416120786c427e2238eb93981e80bc4
kishorgond74 <158150316+kishorgond74@users.noreply.github.com>
2025-07-26 20:02:08 +0000

    dateInJs

---
+++ JavaScript/07_dateInJs.js
@@ -0,0 +1,11 @@
+// Dates 
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2025, 06, 27)
+console.log(myCreatedDate.toDateString());
