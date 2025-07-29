d59d0d449e0ecce1ed9fc95e64baf5d8121decc4
kishorgond74 <158150316+kishorgond74@users.noreply.github.com>
2025-07-29 17:54:37 +0000

    dateInJs

--- JavaScript/07_dateInJs.js
+++ JavaScript/07_dateInJs.js
@@ -7,5 +7,26 @@ let myDate = new Date()
 // console.log(myDate.toLocaleString())
 // console.log(typeof myDate);
 
-let myCreatedDate = new Date(2025, 06, 27)
-console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date(2025, 06, 27)
+let myCreatedDate = new Date("01-14-2023")
+// let myCreatedDate = new Date (2023, 0, 23, 5, 3)
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime())
+console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth() + 1)
+console.log(newDate.getFullYear())
+console.log(newDate.getDay())
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+
+})
