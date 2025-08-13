84b7accff6a0b0fbb83466bcbd16cb2aea30e2ac
kishorgond74 <158150316+kishorgond74@users.noreply.github.com>
2025-08-13 17:24:42 +0000

    objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -4,9 +4,12 @@
 
 const mySym = Symbol("key1")
 
+
+
 const JsUser = {
     name: "Kishor",
     "full name": "Kishor Gond",
+    [mySym]: "mykey1",
     age: 21,
     location: "Nanded",
     email: "gondkishor2@gmail.com",
@@ -14,8 +17,22 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser."full name");
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym])
 
+JsUser.email = "gondk3349@gmail.com"
+// Object.freeze(JsUser)
+JsUser.email = "kishorgond@gmail.com"
+// console.log(JsUser)
 
+JsUser.greetings = function(){
+    console.log("Hello JS User");
+}
+JsUser.greetingstwo = function(){
+    console.log(`Hello JS User, ${this.name}`);
+}
+
+console.log(JsUser.greetings())
+console.log(JsUser.greetingstwo())
