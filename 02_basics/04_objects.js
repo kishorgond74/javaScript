6dcdb63fbac9d37565867ac4f241929b38d2401f
kishorgond74 <158150316+kishorgond74@users.noreply.github.com>
2025-08-15 20:47:33 +0000

    04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,4 +1,4 @@
-// const tinderUser = new Objects()
+                                  // const tinderUser = new Objects()
 const tinderUser = {}
 
 tinderUser.id = "123abc"
@@ -18,6 +18,7 @@ const regularUser = {
 }
 
 // console.log(regularUser.fullname.userfullname.lastname)
+// use of ? after dot is help to avoid if-else condition
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
@@ -26,12 +27,55 @@ const obj4 = {5: "a", 6: "b"}
 // const obj3 = { obj1, obj2}
 // const obj3 = Object.assign( {}, obj1, obj2, obj4)
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2, ...obj4}
 // console.log(obj3)
 
-const users = {
+const users = [
     {
         id: 1,
         email: "h@gmail.com"
-    }
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser)
+
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+
+// console.log(tinderUser.hasOwnProperty('isLogged'))
+
+
+const course = {
+    coursename: 'JS in hindi',
+    courseInstructor: "Hitesh",
+    fees: "999"
 }
+
+course.courseInstructor
+
+const{courseInstructor: abc } = course
+
+// console.log(courseInstructor)
+console.log(abc);
+
+// {
+//     "name": "Hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }      
+
+[
+    {},
+    {},
+    {}
+]
